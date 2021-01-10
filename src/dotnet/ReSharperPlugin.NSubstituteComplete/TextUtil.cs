7ae64d1d5f9bf2c03c534f238a407ce3cccc7981
Socolin <provost.bertrand@gmail.com>
2021-01-10 13:40:43 -0500

    Move tests string completion to another plugin

--- src/dotnet/ReSharperPlugin.NSubstituteComplete/TextUtil.cs
+++ src/dotnet/ReSharperPlugin.NSubstituteComplete/TextUtil.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Text;
 using JetBrains.ReSharper.Psi.JavaScript.Util.Literals;
 using JetBrains.Util;
-using JetBrains.Util.Extension;
 
 namespace ReSharperPlugin.NSubstituteComplete
 {
@@ -10,27 +9,11 @@ namespace ReSharperPlugin.NSubstituteComplete
     {
         public static string ToKebabCase(string input)
         {
-            var tokens = Tokenize(input);
+            var tokens = TokenizeLower(input);
             return string.Join("-", tokens);
         }
 
-        public static string ToCamelCase(string input)
-        {
-            var tokens = Tokenize(input);
-            var sb = new StringBuilder();
-            for (var index = 0; index < tokens.Count; index++)
-            {
-                var token = tokens[index];
-                if (index > 0)
-                    sb.Append(token[0].ToUpperFast()).Append(token.Slice(1, token.Length));
-                else
-                    sb.Append(token);
-            }
-
-            return sb.ToString();
-        }
-
-        private static List<string> Tokenize(string input)
+        public static List<string> TokenizeLower(string input)
         {
             var tokens = new List<string>();
             var sb = new StringBuilder();
